4d4a13736a9c36c0ac48fda04195052785255a0a
zshoop <zachshoop@gmail.com>
2022-03-28 01:43:39 -0400

    The claw can pick things up now

--- Assets/Scripts/ClawScript.cs
+++ Assets/Scripts/ClawScript.cs
@@ -7,10 +7,17 @@ public class ClawScript : MonoBehaviour
 
     Animator m_Animator;
 
+    public GameObject Claw;
+    public Transform dest;
+    private GameObject grabbed = null;
+
     private bool open;
+    private bool close;
     private bool opening;
     private bool closing;
     private bool firstOpen = false;
+    private bool pickup;
+    private bool set;
     private float waitTime = 3f;
 
     // Start is called before the first frame update
@@ -26,43 +33,80 @@ public class ClawScript : MonoBehaviour
         if(Input.GetKey(KeyCode.RightShift)){
             m_Animator.ResetTrigger("Close");
             m_Animator.SetTrigger("Open");
-            if(waitTime > 0 && open == false){
-                opening = true;
-                waitTime -= Time.deltaTime;
-            }
-            if(waitTime == 0){
+            // if(waitTime > 0 && open == false){
+            //     opening = true;
+            //     waitTime -= Time.deltaTime;
+            // }
+            // if(waitTime == 0){
             open = true;
-            opening = false;
-            }
-            firstOpen = true;
-
+            close = false;
+            pickup = false;
+            // opening = false;
+            // }
+            // firstOpen = true;
         }
 
         //close the claw
         if(Input.GetKey(KeyCode.LeftShift)){
             m_Animator.ResetTrigger("Open");
             m_Animator.SetTrigger("Close");
-            if(waitTime > 0 && open == true){
-                closing = true;
-                waitTime -= Time.deltaTime;
-            }
-            if(waitTime == 0){
+            // if(waitTime > 0 && open == true){
+            //     closing = true;
+            //     waitTime -= Time.deltaTime;
+            // }
+            // if(waitTime == 0){
             open = false;
-            closing = false;            
-            }
+            close = true;
+            // closing = false;            
+            // }
         }
+
+        //check if an item has been picked up
+        if(pickup == true){
+                grabbed.GetComponent<MeshCollider>().enabled = false;
+                grabbed.GetComponent<Rigidbody>().useGravity = false;
+                grabbed.transform.position = dest.position;
+                grabbed.transform.parent = GameObject.Find("GrabDest").transform;
+        }
+
+        if(pickup == false){
+            grabbed.transform.parent = null;
+            grabbed.GetComponent<Rigidbody>().useGravity = true;
+            grabbed.GetComponent<MeshCollider>().enabled = true;
+            grabbed = null;
+        }
+    }
+
+    public void SetParent(GameObject newParent)
+    {
+        newParent.GetComponent<MeshCollider>().enabled = false;
+        newParent.GetComponent<Rigidbody>().useGravity = false;
+        newParent.transform.position = dest.position;
+        newParent.transform.parent = GameObject.Find("GrabDest").transform;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.GetComponent<Rigidbody>() != null && closing == true){
-            if(collision.collider.tag == "Piece"){
-                gameObject.AddComponent<FixedJoint>();
-                gameObject.GetComponent<FixedJoint>().connectedBody = collision.rigidbody;
+        if(close == true && grabbed == null){
+            if(collision.collider.tag == "Piece" && pickup == false){
+                // grabbed = collision.gameObject;
+                // grabbed.GetComponent<MeshCollider>().enabled = false;
+                // grabbed.GetComponent<Rigidbody>().useGravity = false;
+                // grabbed.transform.position = dest.position;
+                // grabbed.transform.parent = GameObject.Find("GrabDest").transform;
+                grabbed = collision.gameObject;
+                pickup = true;
+                // gameObject.AddComponent<FixedJoint>();
+                // gameObject.GetComponent<FixedJoint>().connectedBody = collision.rigidbody;
             }
         }
-        if(opening == true && firstOpen == true){
-            Destroy(gameObject.GetComponent<FixedJoint>());
+        if(open == true && grabbed != null){
+            // collision.gameObject.transform.parent = null;
+            // collision.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            // collision.gameObject.GetComponent<MeshCollider>().enabled = true;
+            set = true;
+            pickup = false;
+            // Destroy(gameObject.GetComponent<FixedJoint>());
         }
     }
 }
